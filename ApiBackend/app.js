c08dfe749a5e60ae4c1bb702b0240e1f353e6547
Ernesto Canales <38412674+Ernie-Kanaresu@users.noreply.github.com>
2022-07-12 19:23:37 -0600

    Add files via upload

---
+++ ApiBackend/app.js
@@ -0,0 +1,31 @@
+var passport = require("passport");
+require("./auth/auth");
+require("./database/config");
+
+passport.authenticate("jwt", { session: false })
+
+var userRouter = require("./routers/user");
+var wordRouter = require("./routers/word");
+var authRouter = require("./routers/auth");
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var helmet = require("helmet");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(authRouter);
+app.use(passport.authenticate("jwt", { session: false }))
+app.use("/users", userRouter);
+app.use("/words", wordRouter);
+
+module.exports = app;
